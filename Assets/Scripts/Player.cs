ee237cb259ee83e7ee072ba6eb5993e1539c12b5
Logan-Hipshier-Student <lohi351@student.oaisd.org>
2023-04-24 09:58:51 -0400

    4/24-2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
     [SerializeField] float moveSpeed = .1f;
     Vector2 rawInput;
     Vector2 minBounds;
@@ -33,8 +37,8 @@ public class Player : MonoBehaviour
     {
         Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
         Vector2 newPos= new Vector2();
-        newPos.x=Mathf.Clamp(transform.position.x + delta.x, minBounds.x, maxBounds.x);
-        newPos.y=Mathf.Clamp(transform.position.y + delta.y, minBounds.y, maxBounds.y);
+        newPos.x=Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y=Mathf.Clamp(transform.position.y + delta.y, minBounds.y+paddingBottom, maxBounds.y - paddingTop);
         transform.position = newPos;
     }
 
